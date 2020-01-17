9d2dcad8c1fc35bc9fa1bc2e696d6f366b969f6f
softAdd <soft.snake867@gmail.com>
2020-01-17 19:49:10 +0500

    mobx + decorators

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
 import styles from 'styles/app.module.sass'
+import { observable, computed } from 'mobx'
+import { Provider } from 'mobx-react'
 
-const App = () => (
-    <div className={styles.test}>1</div>
-)
+class App extends Component {
+    @observable price = 3
+    @observable amount = 5
 
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
+    @computed get total() {
+        return this.price * this.amount
+    }
+
+    render() {
+        return (
+            <Provider>
+                <div className={styles.test}>{this.total}</div>
+            </Provider>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+serviceWorker.unregister()
---
+++ src/store/index.js
@@ -0,0 +1,6 @@
+const colors = observable({
+    foreground: '#000',
+    background: '#fff'
+})
+
+export default colors
