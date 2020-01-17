fc3bc51983762ad7bcd9c24b9f0917c32192b5fa
softAdd <soft.snake867@gmail.com>
2020-01-17 19:14:41 +0500

    sass + css modules cra config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import styles from 'styles/app.module.sass'
+
+const App = () => (
+    <div className={styles.test}>1</div>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
