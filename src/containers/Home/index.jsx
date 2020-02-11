78f036c0353a9dd53ee7fb9c28ad9bdc2c445959
softAdd <soft.snake867@gmail.com>
2020-02-11 21:18:17 +0500

    home icons

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -6,8 +6,20 @@ export default () => {
     return (
         <div className={styles.home}>
             <div className={styles.background}></div>
-            <h2 className={styles.title}>MAKSIM KONKOV</h2>
-            <h3 className={styles.subtitle}>WEB-DEVELOPER</h3>
+            <div className={styles.home__content}>
+                <img className={styles.imageTitle} src="/assets/title.svg" alt="page title"></img>
+                <div className={styles.icons}>
+                    <a className={styles.comIcon} href="/">
+                        <img src="/assets/icons/git.svg" alt=""></img>
+                    </a>
+                    <a className={styles.comIcon} href="/">
+                        <img src="/assets/icons/mail.svg" alt=""></img>
+                    </a>
+                    <a className={styles.comIcon} href="/">
+                        <img src="/assets/icons/tel.svg" alt=""></img>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
