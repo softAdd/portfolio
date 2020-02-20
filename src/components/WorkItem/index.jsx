e80946516e5924c667988b94fcad928dcc8af91c
softAdd <soft.snake867@gmail.com>
2020-02-20 22:39:40 +0500

    mobile-first for workItem + focus state for linkButton

--- src/components/WorkItem/index.jsx
+++ src/components/WorkItem/index.jsx
@@ -7,9 +7,7 @@ import LinkButton from 'components/LinkButton'
 export default ({ title, subtitle, image, code, site }) => (
     <div className={styles.workItem}>
         <div className={styles.workItem__content}>
-            <div className={styles.workItem__imageContainer}>
-                <img src={image} alt={`${title}: screenshot`} className={styles.workItem__image} />
-            </div>
+            <div className={styles.workItem__image} style={{ backgroundImage: `url(${image})` }}></div>
             <div className={styles.workItem__description}>
                 <h2 className={styles.workItem__title}>{title}</h2>
                 <p className={styles.workItem__subtitle}>{subtitle}</p>
